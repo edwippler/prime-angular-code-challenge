7922f1dbe642e76202afe7554a4d9ec258988b84
Edmund Wippler <ed.wippler@gmail.com>
2017-03-13 13:30:04 -0500

    heroList ajax call working and displaying on hero-list view

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,14 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+    var self = this;
+    self.message = 'awwee yeah!';
+    self.herosList = [];
+
+    $http({
+      method: 'GET',
+      url: '/heroes'
+    }).then(function(response){
+      // console.log(response.data);
+      self.herosList = response.data;
+    });
+}]);
